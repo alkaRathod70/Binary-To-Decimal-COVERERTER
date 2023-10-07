5efc8db8f1277f46652a0a36f7717a83b892f9b9
alkaRathod70 <alkarathod2020@gmail.com>
2023-10-07 13:20:59 +0530

    create index.html
    this binary to decimal files.

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function fun() {
+    var a= document.getElementById('bin').value;
+    var b=0;
+    var i=0;
+    while(a){
+        b=b+Math.pow(2,i)*(a%10);
+        i++;
+        a=parseInt(a/10);
+   
+   
+    }
+    document.getElementById('dec').value=b;
+    return false;
+   
+   
+   }
